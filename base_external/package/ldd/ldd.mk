df5bf2b5f85e6b24dc13dad278acd41d4be0b9ae
Vimal101091 <si.vimal@outlook.com>
2025-12-28 19:07:44 +0530

    Assignment 7 part 2

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,14 @@
+﻿################################################################################
+#
+# ldd
+#
+################################################################################
+
+LDD_VERSION = b00723e83fda3ca12b7b0fc23b04fd869a2ebe67
+LDD_SITE = https://github.com/cu-ecen-aeld/assignment-7-Vimal101091.git
+LDD_SITE_METHOD = git
+
+LDD_MODULE_SUBDIRS = misc-modules scull
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
