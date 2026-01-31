c73accd9434aa7a94bb86d7dba1007e4f5750177
Vimal Si <68633784+Vimal101091@users.noreply.github.com>
2026-01-31 15:28:56 +0530

    Define installation commands for scull module
    Added installation commands for scull module.

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,14 +1,17 @@
-﻿################################################################################
+################################################################################
 #
 # ldd
 #
 ################################################################################
-
 LDD_VERSION = assignment-7-buildroot
 LDD_SITE = https://github.com/cu-ecen-aeld/assignment-7-Vimal101091.git
 LDD_SITE_METHOD = git
-
 LDD_MODULE_SUBDIRS = misc-modules scull
+
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
