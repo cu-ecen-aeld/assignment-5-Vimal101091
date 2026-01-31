5df62dcb070687fa8fb979e2ae87beaf8151af6a
Vimal Si <68633784+Vimal101091@users.noreply.github.com>
2026-01-31 15:58:49 +0530

    Change LDD_SITE to use SSH instead of HTTPS

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 LDD_VERSION = assignment-7-buildroot
-LDD_SITE = https://github.com/cu-ecen-aeld/assignment-7-Vimal101091.git
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-Vimal101091.git
 LDD_SITE_METHOD = git
 LDD_MODULE_SUBDIRS = misc-modules scull
 
